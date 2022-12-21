671b603581f6d021c50735f371873a9cf7b4b435
matada2000 <matada200060@gmail.com>
2022-12-21 08:57:09 +0100

    v1.0

---
+++ Inwentaryzacja_ionic/src/app/guards/auth.guard.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard {
+  
+}
