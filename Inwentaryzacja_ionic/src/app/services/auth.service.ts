7aa54f15e6407ba27304bee78e82d07bc9e7278f
matada2000 <matada200060@gmail.com>
2022-12-21 09:48:02 +0100

    v1.10 - jwt

--- Inwentaryzacja_ionic/src/app/services/auth.service.ts
+++ Inwentaryzacja_ionic/src/app/services/auth.service.ts
@@ -1,9 +1,74 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Platform } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+import { BehaviorSubject, Observable, from, of } from 'rxjs';
+import { take, map, switchMap } from 'rxjs/operators';
+
+const helper = new JwtHelperService();
+const TOKEN_KEY = 'jwt-token';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public user: Observable<any>;
+  private userData = new BehaviorSubject(null);
 
-  constructor() { }
+  constructor(private storage: Storage, private http: HttpClient, private plt: Platform, private router: Router) {
+    this.loadStoredToken();
+  }
+
+  loadStoredToken() {
+    let platformObs = from(this.plt.ready());
+    this.user = platformObs.pipe(
+      switchMap(() => {
+        return from(this.storage.get(TOKEN_KEY))
+      }),
+      map(token => {
+        console.log('Token from storage: ', token);
+        if (token) {
+          let decoded = helper.decodeToken(token);
+          console.log('decded: ', decoded);
+          this.userData.next(decoded);
+        } else {
+          return null;
+        }
+      })
+    );
+  }
+
+  login(credentials: {email: string, pw: string}): Observable<any> {
+    if (credentials.email != 'test@gmail.com' || credentials.pw != '123') {
+      return null;
+    }
+
+    return this.http.get('https://randomuser.me/api/').pipe(
+      take(1),
+      map(res => {
+        return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+      }),
+      switchMap(token => {
+        let decoded = helper.decodeToken(token);
+        console.log('login decded: ', decoded);
+        this.userData.next(decoded);
+
+        let storageObs = from(this.storage.set(TOKEN_KEY, token));
+        return storageObs;
+      })
+    );
+  }
+  getUser() {
+    return this.userData.getValue();
+  }
+
+  logout() {
+    this.storage.remove(TOKEN_KEY).then(() => {
+      this.router.navigateByUrl('/');
+      this.userData.next(null);
+    })
+  }
+
 }
